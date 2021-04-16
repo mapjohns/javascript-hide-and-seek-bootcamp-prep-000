24297b57b33da0f68c816297c426c5826e656170
Matthew Johnson <mattj20@gmail.com>
2021-04-16 01:32:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function increaseRankBy(n) {
+  let newVar = document.querySelectorAll('.ranked-list li')
+  let  i = 0
+  for (i=0; i<newVar.length;i++) {
+    newVar[i].innerHTML = parseInt(newVar[i].innerHTML) + n
+  }
+  return newVar
+}
 
+
+function deepestChild() {
+  
+}
